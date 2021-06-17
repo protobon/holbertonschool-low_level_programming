866be1e4f257ace6a592bb2c6bb5b270f85a400c
root <3282@holbertonschool.com>
2021-06-17 16:38:05 -0700

    Project 0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using printf to display text on screen
+ * Return: returns value 0
+*/
+int main(void)
+{
+	char myStr[] = "with proper grammar, but the outcome is a piece of art,";	
+	printf(myStr);
+	return (0);
+}
