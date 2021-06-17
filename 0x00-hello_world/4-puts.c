7737f7bfa0ba6ed59c04a56bac355c94a1292089
root <3282@holbertonschool.com>
2021-06-17 16:23:34 -0700

    Project 0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
 /**
- * Using puts inside 
- * main to print 
- * text on screen 
+ * main - using puts function to print text on screen
+ * Return: program ends with the value 0
  */
 int main(void)
 {
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
-
 	return (0);
 }
