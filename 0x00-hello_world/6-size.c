229918945b89546ded4fe388b7aefe983d9f27cb
root <3282@holbertonschool.com>
2021-06-17 17:01:50 -0700

    Project 0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - using printf to display size of types
+ * Return: returns value 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
