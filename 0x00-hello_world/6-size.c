afcfb331506f2da7c15ec1d5a29862a090e2c681
root <3282@holbertonschool.com>
2021-06-17 17:16:52 -0700

    Project 0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,11 @@
  */
 int main(void)
 {
-	fprintf(stderr, "Size of a char: 1 byte(s)\n");
+	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 4 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
+	fprintf(stderr, "error");
 	return (0);
 }
