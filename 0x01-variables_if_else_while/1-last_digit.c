d5052712d6fa9fb5af622550601c5f473eafec78
root <3282@holbertonschool.com>
2021-06-18 09:11:11 -0700

    Project 0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the last digit of a random variable n 
+ * main - prints the last digit of a random variable n
  * Return: returns 0
  */
 int main(void)
 {
 	int n, last;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	} else
 	{
-		printf("Last digit of %d is %d and is 0", n, last);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-       	return (0);
+	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
  * main - prints out the alphabet in lower case
- * return: returns 0
+ * Return: returns 0
  */
-int main (void)
+int main(void)
 {
-	char ch, nl;
-	nl = '\n';
-	
+	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar(nl);
+	putchar('\n');
 	return (0);
 }
