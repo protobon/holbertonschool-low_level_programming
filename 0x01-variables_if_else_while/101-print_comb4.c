809725bd977ce7d16986180055c4843b5ac47204
root <3282@holbertonschool.com>
2021-06-18 18:44:46 -0700

    Project: 0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints every possible combination between two digits
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				if (a >= b || a >= c || b >= c)
+				{
+					continue;
+				}
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a == 7 && b == 8 && c == 9)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
