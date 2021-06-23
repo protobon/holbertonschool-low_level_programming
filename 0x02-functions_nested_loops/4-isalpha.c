dcdbc03a136bce230143b1fef9cb56c1cd78cd31
root <3282@holbertonschool.com>
2021-06-22 17:57:48 -0700

    Project 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - check if input is an alphabetic letter
+ * @c:character
+ * Return: returns 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	int i;
+
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		i = 1;
+	} else
+	{
+		i = 0;
+	}
+	return (i);
+}
