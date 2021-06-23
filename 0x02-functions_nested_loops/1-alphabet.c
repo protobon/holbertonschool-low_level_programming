1cb949150c2984c205e37fd3d31a69d56e858e0f
root <3282@holbertonschool.com>
2021-06-22 17:18:49 -0700

    Project 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - prints alphabet lowercase followed by a new line
+ * Return: returns 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
