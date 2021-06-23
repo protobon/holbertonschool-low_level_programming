547d53eee3928d311de528bc9eb03612456c7955
root <3282@holbertonschool.com>
2021-06-23 09:59:17 -0700

    Project 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+ * print_times_table - prints n times
+ *@n: integer input
+ *
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if ((n < 15) && (n > 0))
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (c > 99)
+				{
+					_putchar((c / 100) + '0');
+					c = c / 10;
+					_putchar((c % 10) + '0');
+				} else if (c > 9)
+				{
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+				} else if (c < 10 && b > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((c % 10) + '0');
+				if (b < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
