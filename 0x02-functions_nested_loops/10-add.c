8ca30da36fb7a47be56f72539e2a2a1365ee24b0
root <3282@holbertonschool.com>
2021-06-22 19:45:26 -0700

    Project 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - returns the addition of two integers
+ * @a: integer
+ * @b: integer
+ * Return: returns int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
