bbf57bceb951846c053416e5023b34d4a34ea5b5
root <3282@holbertonschool.com>
2021-06-24 14:51:06 -0700

    Project 0x03 C - Debugging

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,6 +5,9 @@
 
 void positive_or_negative(int i);
 
-#endif /* HOLBERTON_H */
-
 int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+#endif /* HOLBERTON_H */
