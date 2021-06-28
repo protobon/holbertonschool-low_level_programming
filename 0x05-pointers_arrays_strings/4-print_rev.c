a0d7614755534d7512e78a27e82d6a2eba8b8eb6
root <3282@holbertonschool.com>
2021-06-28 16:50:33 -0700

    Project 0x05 C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string followed by a new line
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
