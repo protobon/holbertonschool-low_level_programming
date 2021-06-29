d3ab2b8cdd36ce1c47746b437b32bc8131902251
root <3282@holbertonschool.com>
2021-06-28 18:27:02 -0700

    Project 0x05 C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
