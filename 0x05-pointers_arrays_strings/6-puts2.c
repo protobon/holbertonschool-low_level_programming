138f04f52b2e7476fcd24e6462acfe88e31f4fb1
root <3282@holbertonschool.com>
2021-06-28 18:40:19 -0700

    Project 0x05 C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != 0)
+		if ((i % 2) == 0)
 		{
 			_putchar(str[i]);
 		}
