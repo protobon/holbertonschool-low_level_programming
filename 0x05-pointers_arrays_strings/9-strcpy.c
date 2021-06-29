9032ab140af3b83df6abb11c1d31973af845f301
root <3282@holbertonschool.com>
2021-06-29 06:46:08 -0700

    Project 0x05 C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,19 @@
 #include "holberton.h"
 
 /**
- *_strcpy - print a message with printf
- *@dest : number
- *@src : number
- *
- *Return: end program
+ * _strcpy - copies a string and saves it to another string
+ * @dest : string
+ * @src : string
+ * Return: returns string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (*(src + i) != 0)
+	while (src[i] != '\0')
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 		i++;
 	}
-	*(dest + i) = *(src + i);
 	return (dest);
 }
