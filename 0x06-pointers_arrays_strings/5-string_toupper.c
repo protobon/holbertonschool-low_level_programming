64db216dd4df6b70d7e946e31ff7adf1db189148
root <3282@holbertonschool.com>
2021-07-01 10:14:15 -0700

    Project 0x06 C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - changes every character of string to uppercase
+ * Return: string
+ * @str: string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
