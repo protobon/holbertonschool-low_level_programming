d1d383b1138d33c6c0687ff2cd76abcac0285239
root <3282@holbertonschool.com>
2021-07-02 08:58:11 -0700

    Project 0x06 C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,19 +8,20 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
-	char* *p;
+	char **p;
 
 	p = &dest;
 
 	while (dest[i] != '\0')
 	{
 		i++;
+		j++;
 	}
-	if (i < n)
+	if (n > j)
 	{
-		n = i;
+		n = j;
 	}
-	for (; j < n; j++)
+	for (j = 0; j < n; j++)
 	{
 		dest[i] = src[j];
 		i++;
