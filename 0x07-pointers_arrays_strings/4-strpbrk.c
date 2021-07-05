6f4df961ba01850e4a6e74fa927c66fad630dfaf
root <3282@holbertonschool.com>
2021-07-05 11:31:12 -0700

    Project 0x07 C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+ * _strpbrk - locates first occurance in s and prints from that point
+ * @s: string
+ * @accept: string
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (j = 0; accept[j] != 0; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (0);
+}
