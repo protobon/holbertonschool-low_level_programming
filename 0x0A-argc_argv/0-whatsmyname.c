9e8dc78e560a7a9eedc4718e68aa3e4b1a7dd328
root <3282@holbertonschool.com>
2021-07-09 09:13:20 -0700

    Project 0x0A C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the programs name
+ * Return: 0
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
