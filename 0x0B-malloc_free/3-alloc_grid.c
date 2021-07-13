f14be5251bf9d7c50c55aa614f1fe7fcf672b7b5
root <3282@holbertonschool.com>
2021-07-13 13:58:23 -0700

    Project 0x0B C - malloc, free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a dynamic matrix
+ * @width: int
+ * @height: int
+ * Return: pointer to matrix
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p1 = NULL;
+	int *p2 = NULL;
+	int i = 0, j = 0;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (p1);
+	}
+	p1 = malloc(sizeof(int *) * height);
+	if (p1)
+	{
+		for (; i < height; i++)
+		{
+			p2 = malloc(sizeof(int) * width);
+			if (p2)
+			{
+				for (; j < width; j++)
+				{
+					p2[j] = 0;
+				}
+			}
+			p1[i] = p2;
+		}
+	}
+	return (p1);
+}
