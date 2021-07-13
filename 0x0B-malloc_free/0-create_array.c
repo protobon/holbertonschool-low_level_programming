dac3fd3901923e6c3e2a14ff7806e245e3268fc3
root <3282@holbertonschool.com>
2021-07-13 11:52:16 -0700

    Project 0x0B C - malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates a string of size with c repeated
+ * @size: unsigned int
+ * @c: char
+ * Return: pointer to string
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p = NULL;
+	unsigned int i = 0;
+
+	p = malloc(sizeof(char) * size + 1);
+	if (p)
+	{
+		for (; i < size; i++)
+		{
+			p[i] = c;
+		}
+		p[i] = '\0';
+	}
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Project 0x0B C - malloc, free
