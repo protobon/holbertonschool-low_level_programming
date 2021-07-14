a59ef8b93df2aca04b5ebd85e22965185e648636
root <3282@holbertonschool.com>
2021-07-13 18:33:56 -0700

    Project 0x0B C - malloc, free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: 2 dim array
+ * @height: int
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
