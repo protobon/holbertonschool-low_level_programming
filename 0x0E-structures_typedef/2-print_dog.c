422c8cbb3f958289f0b3380a14af500c4d7cdd86
root <3282@holbertonschool.com>
2021-07-19 16:10:15 -0700

    Project 0x0E C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,35 +4,20 @@
 /**
  * print_dog - prints dog's specs
  * @d: struct dog
+ * Return: dog
  */
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
 		if (d->name)
-		{
 			printf("Name: %s\n", d->name);
-		}
 		else
-		{
 			printf("Name: (nil)\n");
-		}
-		if (d->age)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)\n");
-		}
+		printf("Age: %f\n", d->age);
 		if (d->owner)
-		{
 			printf("Owner: %s\n", d->owner);
-		}
 		else
-		{
 			printf("Owner: (nil)\n");
-		}
-
 	}
 }
