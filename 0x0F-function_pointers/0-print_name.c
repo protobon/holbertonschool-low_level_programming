5a8ddbf2c8be2a77687918ffb0f6fd988d42227d
hippocampus3282 <3282@holbertonschool.com>
2021-07-21 12:40:44 -0700

    Project 0x0F C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,11 @@
  * print_name - prints name
  * @name: string, name
  * @f: pointer to function
+ * Description: prints a name
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f && name)
-	{
 		f(name);
-	}
 }
