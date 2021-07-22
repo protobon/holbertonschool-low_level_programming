8704b631052fa62063be1a7719cdfb1a6c3ac07c
hippocampus3282 <3282@holbertonschool.com>
2021-07-22 15:04:13 -0700

    Project 0x10 C - Variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all parameters
+ * @n: number of elements
+ * Return: int, sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Project 0x10 C - Variadic functions
