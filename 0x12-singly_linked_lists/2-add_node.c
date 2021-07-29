1ce3520624d0b9ef28ce8901b35991fc866c1454
hippocampus3282 <3282@holbertonschool.com>
2021-07-29 16:03:44 -0700

    Project 0x12 C - Linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+  * add_node - adds a new node at the beginning of list
+  * @head: pointer to new node
+  * @str: pointer to string within node
+  * Return: pointer to head of list
+  */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *scopy = NULL;
+	list_t *ptr = NULL;
+
+	if (str)
+		scopy = strdup(str);
+	ptr = malloc(sizeof(list_t));
+	if (ptr && head)
+	{
+		ptr->str = scopy;
+		ptr->len = strlen(scopy);
+		ptr->next = *head;
+		*head = ptr;
+	}
+	return (ptr);
+}
