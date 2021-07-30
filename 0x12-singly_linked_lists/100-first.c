9c132782699566c102a6943214222aaa72dba88f
hippocampus3282 <3282@holbertonschool.com>
2021-07-29 17:45:32 -0700

    Project 0x12 C - Linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+  * print_before_main - prints text before main is executed
+  */
+void __attribute__((constructor)) print_before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
