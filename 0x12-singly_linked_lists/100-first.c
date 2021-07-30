e17fe093ce89498b8e24e1845983c569150e3fc5
hippocampus3282 <3282@holbertonschool.com>
2021-07-29 17:46:47 -0700

    Project 0x12 C - Linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,5 +4,6 @@
   */
 void __attribute__((constructor)) print_before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
