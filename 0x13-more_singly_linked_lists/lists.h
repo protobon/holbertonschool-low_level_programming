0a6b890ecc1d59ec94d8ac675b834e1b3276981b
hippocampus3282 <3282@holbertonschool.com>
2021-08-03 12:24:43 -0700

    Project 0x13 C - More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,12 +8,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
