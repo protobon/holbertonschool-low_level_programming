174b9402041d901e7d1e4a75615d9a8d324c5d4b
hippocampus3282 <3282@holbertonschool.com>
2021-08-03 17:15:26 -0700

    Project 0x13 C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 
 /**
+  * create_node - creates new listint_t node
+  * @n: data of new node
+  * Return: pointer to new node
+  */
+listint_t *create_nodeint(int n)
+{
+	listint_t *new = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (new)
+	{
+		new->n = n;
+		new->next = NULL;
+	}
+	return (new);
+}
+
+/**
+  * get_nodeint_at_index - gets the nth node of a list
+  * @head: pointer to head node
+  * @index: index of node to return
+  * Return: pointer to indexed node
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	if (!head && index)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	return (get_nodeint_at_index(head->next, index - 1));
+}
+
+/**
   * insert_nodeint_at_index - inserts new node at nth index
   * @head: pointer to pointer to head of list
   * @idx: index to add new node to
@@ -10,32 +43,30 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new = NULL, *temp1 = NULL, *temp2 = NULL;
-	unsigned int i = 0;
+	listint_t *new = NULL, *prev = NULL;
 
 	if (*head)
 	{
-		temp1 = *head;
-		new = malloc(sizeof(listint_t));
-		if (new)
-		{
-			new->n = n;
-			for (; i < idx - 1; i++)
-			{
-				temp1 = temp1->next;
-			}
-			temp2 = temp1->next;
-			new->next = temp2;
-			while (temp2->next)
-			{
-				temp2 = temp2->next;
-			}
-			temp1->next = new;
-		}
+		new = create_nodeint(n);
+		if (!new)
+			return (NULL);
+		new->next = get_nodeint_at_index(*head, idx);
+		if (!idx)
+			*head = new;
 		else
 		{
-			return (NULL);
+			prev = get_nodeint_at_index(*head, idx - 1);
+			if (!prev)
+			{
+				free(new);
+				return (NULL);
+			}
+			prev->next = new;
 		}
+	}
+	else
+	{
+		return (NULL);
 	}
 	return (new);
 }
