982f6fe635c390a6fbf94ff2af4be98a84c42ed0
hippocampus3282 <3282@holbertonschool.com>
2021-08-04 06:49:23 -0700

    Project 0x13 C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -50,7 +50,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new = create_nodeint(n);
 		if (!new)
 			return (NULL);
-		new->next = get_nodeint_at_index(*head, idx);
+		new->next = get_nodeint_at_index(*head, idx + 1);
 		if (!idx)
 			*head = new;
 		else
