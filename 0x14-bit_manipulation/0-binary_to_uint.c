046b0ca48e3f7d39af67b30e56f987a42ee4e5a9
hippocampus3282 <3282@holbertonschool.com>
2021-08-05 17:03:07 -0700

    Project 0x14 C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum = 0, p = 1;
 
 	len = strlen(b);
-	if (!b)
+	if (!b || !len)
 		return (sum);
 	for (i = len - 1; i >= 0; i--, p *= 2)
 	{
