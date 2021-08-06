4d12b8683f6446665d503fa99aebadd4302c5531
hippocampus3282 <3282@holbertonschool.com>
2021-08-05 18:30:09 -0700

    Project 0x14 C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
