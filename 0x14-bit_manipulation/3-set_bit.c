44c100102a8378053636a995f42d56262eab0ad4
hippocampus3282 <3282@holbertonschool.com>
2021-08-06 09:56:21 -0700

    Project 0x14 C - Bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * set_bit - sets bit as 1 at index
+  * @n: decimal number, input
+  * @index: index
+  * Return: 1, -1 if error
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (!n || index > 64)
+		return (-1);
+	else
+	{
+		*n = *n | (1 << index);
+	}
+	return (1);
+}
