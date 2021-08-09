7087cfdfa21fe489c915d9e3d83fa543a05cdb30
hippocampus3282 <3282@holbertonschool.com>
2021-08-09 10:27:04 -0700

    Project 0x15 C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 
