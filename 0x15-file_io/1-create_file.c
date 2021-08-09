f79d33fce6648238589555e0112b7b713f312699
hippocampus3282 <3282@holbertonschool.com>
2021-08-09 10:36:59 -0700

    Project 0x15 C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,15 +14,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
 		len = strlen(text_content);
-		w = write(fd, text_content, len);
 	}
+	w = write(fd, text_content, len);
+
 	close(fd);
 
 	if (w != len)
