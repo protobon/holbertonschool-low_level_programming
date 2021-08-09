71e8f5a05e5679c74fd03f90f0dd66ca7deb2f4b
hippocampus3282 <3282@holbertonschool.com>
2021-08-09 11:01:04 -0700

    Project 0x15 C - File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * append_text_to_file - appends text t ofile
+  * @filename: file to append text to
+  * @text_content: content to append on file
+  * Return: 1 if success, -1 if fails
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w, len;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		len = strlen(text_content);
+	}
+	w = write(fd, text_content, len);
+
+	close(fd);
+
+	if (w != len || w == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
