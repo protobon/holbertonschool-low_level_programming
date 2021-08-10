554be13036cf8fbfc5d88fc666f611f812a2752a
hippocampus3282 <3282@holbertonschool.com>
2021-08-10 09:47:26 -0700

    Project 0x15 C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #define BUF_SIZE 1024
 
 /**
-  * cp_file - copies the content of one file to another
+  * main - copies the content of one file to another
   * @argc: int, argument count
   * @argv: array of arguments
   * Return: always 0
@@ -15,15 +15,9 @@ int main(int argc, char **argv)
 	ssize_t r = 1024, w;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (!argv[1])
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	f_from = open(argv[1], O_RDONLY);
 	f_to = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
 	if (f_to < 0)
@@ -38,20 +32,11 @@ int main(int argc, char **argv)
 		}
 		w = write(f_to, buf, r);
 		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(f_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from), exit(100);
 	if (close(f_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to), exit(100);
 	return (0);
 }
