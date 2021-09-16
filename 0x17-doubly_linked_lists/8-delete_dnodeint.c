840e5554b0ee84871d4a107087686376e7fa8e6b
hippocampus3282 <3282@holbertonschool.com>
2021-09-16 15:19:20 -0700

    Project 0x17 C - Doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,27 +1,6 @@
 #include "lists.h"
 
 /**
-  * get_dnodeint_at_index - gets node of dlistint at idx
-  * @head: head of list
-  * @index: index node
-  * Return: address of node at index
-  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	dlistint_t *tmp = head;
-	unsigned int i = 0;
-
-	if (index == 0)
-		return (head);
-	while (i < index && tmp)
-	{
-		i++;
-		tmp = tmp->next;
-	}
-	return (tmp);
-}
-
-/**
   * delete_dnodeint_at_index - deletes node at given index
   * @head: head of node
   * @index: index to delete node
@@ -42,7 +21,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(del);
 		return (1);
 	}
-	del = get_dnodeint_at_index(*head, index);
+	while (index > 0)
+	{
+		del = del->next;
+		if (!del)
+			return (-1);
+		index--;
+	}
 	if (del == *head)
 		*head = del->next;
 
