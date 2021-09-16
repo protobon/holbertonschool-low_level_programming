49bf0c83ae328abb2706e988f2875c941d7cfbc8
hippocampus3282 <3282@holbertonschool.com>
2021-09-16 08:48:16 -0700

    Project 0x17 C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,21 @@
 #include "lists.h"
 
+/**
+  * print_dlistint - prints all elements of a dlistint
+  * @h: head of list
+  * Return: number of nodes
+  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *tmp = NULL;
-    size_t i = 0;
+	const dlistint_t *tmp = NULL;
+	size_t i = 0;
 
-    tmp = h;
-    while (tmp)
-    {
-        printf("%d\n", tmp->n);
-        tmp = tmp->next;
-        i++;
-    }
-    return (i);
+	tmp = h;
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
 }
