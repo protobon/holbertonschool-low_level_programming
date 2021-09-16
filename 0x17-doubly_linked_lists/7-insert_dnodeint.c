8eac0fefa2a3b0264c952dae9852b61caac48a16
hippocampus3282 <3282@holbertonschool.com>
2021-09-16 15:35:51 -0700

    Project 0x17 C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,7 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new);
 		return (add_dnodeint_end(h, n));
 	}
-	else
+	else if (count > idx)
 		return (NULL);
 	return (new);
 }
