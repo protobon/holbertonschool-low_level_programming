2fb840b67f9102596244c9e4c53a55fcf7b259d4
hippocampus3282 <3282@holbertonschool.com>
2021-10-07 16:32:22 -0700

    Project 0x1A C - Hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,10 +10,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx;
 	char *value;
+	hash_node_t *tmp;
 
 	idx = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[idx] == NULL)
 		return (NULL);
-	value = strdup(ht->array[idx]->value);
+	tmp = ht->array[idx];
+	while (tmp->key != key)
+		tmp = tmp->next;
+	value = strdup(tmp->value);
 	return (value);
 }
