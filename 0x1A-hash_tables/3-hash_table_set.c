0c9f7451dd7b37fa57e8e2c30ed60e54419b5c7f
hippocampus3282 <3282@holbertonschool.com>
2021-10-08 07:27:44 -0700

    Project 0x1A C - Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	idx = key_index((unsigned char *)key, ht->size);
 	if (ht->array[idx] != NULL)
 	{
-		if (ht->array[idx]->key == key)
+		if (strcmp(ht->array[idx]->key, key) == 0)
 		{
 			free(new);
 			ht->array[idx]->value = strdup(value);
