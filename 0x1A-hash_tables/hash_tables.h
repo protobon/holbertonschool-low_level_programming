2e34d06559e28c33232c2920f7a993dcd3a1a6bc
hippocampus3282 <3282@holbertonschool.com>
2021-10-13 20:41:28 -0700

    Project 0x1A C - Hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -50,7 +50,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-shash_node_t *insert_node(shash_node_t **h, const char *k, const char *v);
 void sorted_list_insert(shash_table_t *ht, shash_node_t *node);
 
 /* ------ HASH TABLE ------ */
